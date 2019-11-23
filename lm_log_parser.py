e7afbbea2389c6626a09ca01f174b4559bfde436
yottabytt <yottabytt@gmail.com>
2019-11-23 16:59:14 -0500

    made changes to viz to support different log files

--- lm_log_parser.py
+++ lm_log_parser.py
@@ -5,9 +5,10 @@ class LMLogParser:
     def __init__(self, file_loc, n_splits_train,
                     n_splits_valid, n_splits_test, loss_o_pos_train,
                     loss_o_pos_valid, loss_o_pos_test, val_i_pos_train,
-                    val_i_pos_valid, val_i_pos_test):
+                    val_i_pos_valid, val_i_pos_test, version='my_mos'):
 
         self.file_loc = file_loc
+        self.version = version
         self.n_splits_train = n_splits_train
         self.n_splits_valid = n_splits_valid
         self.n_splits_test = n_splits_test
@@ -21,7 +22,9 @@ class LMLogParser:
             'intvl_train_loss': [],
             'intvl_train_ppl': [],
             'epoch_valid_loss': [],
-            'epoch_valid_ppl': []
+            'epoch_valid_ppl': [],
+            'epoch_train_loss': [],
+            'epoch_train_ppl': []
         }
         
     def parse_train_log(self, splits):
@@ -32,23 +35,37 @@ class LMLogParser:
             splits[self.loss_o_pos_train + 1].split()[self.val_i_pos_train].strip()
         )
 
-    def parse_valid_log(self, splits):
+
+    def parse_valid_log(self, splits, version):
         self.parsed['epoch_valid_loss'].append(
-            splits[self.loss_o_pos_valid].split()[self.val_i_pos_valid].strip()
+            splits[self.loss_o_pos_valid].strip().split()[self.val_i_pos_valid]
         )
         self.parsed['epoch_valid_ppl'].append(
-            splits[self.loss_o_pos_valid + 1].split()[self.val_i_pos_valid].strip()
+            splits[self.loss_o_pos_valid + 1].strip().split()[self.val_i_pos_valid]
         )
+
+        if version == 'tcc_hsm':
+            self.parsed['epoch_train_loss'].append(
+                splits[self.loss_o_pos_train].strip().split()[self.val_i_pos_train]
+            )
+            self.parsed['epoch_train_ppl'].append(
+                splits[self.loss_o_pos_train + 1].strip().split()[self.val_i_pos_train]
+            )
 
     def parse(self):
         with open(self.file_loc, 'r') as f:
             for l in f:
                 splits = re.split(r'\|', l)
                 n_splits = len(splits)
-                if(n_splits == self.n_splits_train):
-                    self.parse_train_log(splits)    
-                elif(n_splits == self.n_splits_valid):
-                    self.parse_valid_log(splits)
+                if self.version == 'tcc_hsm':
+                    if(n_splits == self.n_splits_valid and
+                        'end' in splits[2]):
+                        self.parse_valid_log(splits, self.version)
+                else:
+                    if(n_splits == self.n_splits_train):
+                        self.parse_train_log(splits)    
+                    elif(n_splits == self.n_splits_valid):
+                        self.parse_valid_log(splits, self.version)
 
 if __name__ == '__main__':
     
--- viz_wrapper.py
+++ viz_wrapper.py
@@ -7,18 +7,32 @@ from viz import Viz
 class VizWrapper:
 
     @staticmethod
-    def get_lmlp(file_loc):
+    def get_lmlp(file_loc, version='my_mos'):
+
+        if version == 'my_mos':
+            n_splits_valid = 6
+            loss_o_pos_train = 6
+            loss_o_pos_valid = 4
+            val_i_pos_train = 1
+            val_i_pos_valid = 2
+        elif version == 'tcc_hsm':
+            n_splits_valid = 8
+            loss_o_pos_train = 4
+            loss_o_pos_valid = 6
+            val_i_pos_train = 3
+            val_i_pos_valid = 2
 
         return LMLogParser(
                 file_loc = file_loc,
+                version = version,
                 n_splits_train = 8,
-                n_splits_valid = 6,
+                n_splits_valid = n_splits_valid,
                 n_splits_test = 5,
-                loss_o_pos_train = 6,
-                loss_o_pos_valid = 4,
+                loss_o_pos_train = loss_o_pos_train,
+                loss_o_pos_valid = loss_o_pos_valid,
                 loss_o_pos_test = 3,
-                val_i_pos_train = 1,
-                val_i_pos_valid = 2,
+                val_i_pos_train = val_i_pos_train,
+                val_i_pos_valid = val_i_pos_valid,
                 val_i_pos_test = 1
             )
 
@@ -27,9 +41,12 @@ class VizWrapper:
 
         lmlp.parse()
         
-        intvl_train_loss = [float(x) for x in lmlp.parsed['intvl_train_loss']]
-        epoch_train_loss = [statistics.mean(intvl_train_loss[x:x+intvl_len]) for x in range(0, len(intvl_train_loss),intvl_len)]
-        epoch_train_ppl = [math.exp(x) for x in epoch_train_loss]
+        if lmlp.version == 'my_mos':
+            intvl_train_loss = [float(x) for x in lmlp.parsed['intvl_train_loss']]
+            epoch_train_loss = [statistics.mean(intvl_train_loss[x:x+intvl_len]) for x in range(0, len(intvl_train_loss),intvl_len)]
+            epoch_train_ppl = [math.exp(x) for x in epoch_train_loss]
+        elif lmlp.version == 'tcc_hsm':
+            epoch_train_ppl = [float(x) for x in lmlp.parsed['epoch_train_ppl']]
         
         epoch_valid_ppl = [float(x) for x in lmlp.parsed['epoch_valid_ppl']]
 
